710cb884918800d797cab70df85bfacff70aeb79
Ehtisham Ahmad Kisana <ehtishamkisana@gmail.com>
2023-03-02 15:56:39 +0500

    size handler method createed

--- components/Eyecomponents/SnellersMethod/Snellers.js
+++ components/Eyecomponents/SnellersMethod/Snellers.js
@@ -1,93 +1,130 @@
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { useState } from 'react';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { useState } from "react";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 const COLORS = {
-  dark: '#041C32',
-  primary: '#04293A',
-  secondary: '#064663',
-  light: '#ECB365',
-
+  dark: "#041C32",
+  primary: "#04293A",
+  secondary: "#064663",
+  light: "#ECB365",
 };
 
-
 const Snellers = (navigation) => {
-  
-  const [size, setSize] = useState(75);  
+  const [size, setSize] = useState(75);
   const [index, setIndex] = useState(0);
-  const [alpha, setAlpha] =useState(
-    ["E","F  P", "T  O  Z", "L P E D", "P E C F D",
-     "E D F C Z P", "D E F P 0 T E C"]
-  );
-   
-  
-  const Nextslide=()=>{
-    if(index===6){
-      setIndex(0);
-    }
-    else{
-      setIndex(index+1)
-    }
+  const [alpha, setAlpha] = useState([
+    "E",
+    "F  P",
+    "T  O  Z",
+    "L   P   E   D",
+    "P   E   C   F   D",
+    "E    D    F    C    Z    P",
+    "D     E     F     P     O     T     E     C",
+  ]);
+
+  const sizeHandler = () => {
+    switch (index) {
+      case 0:
+        return setSize(65);
+      
+      case 1:
+        return setSize(55);
+       
+      case 2:
+        return setSize(45);
     
-  }
+      case 3:
+        return setSize(35);
+        
+      case 4:
+        return setSize(25);
+       
+      case 5:
+        return setSize(15);
+     
+      
+    
+      default:
+        return setSize(75);
+          
+    }
+  };
+
+
+  const Nextslide = () => {
+    if (index === 6) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+    sizeHandler();
+  };
 
   return (
     <View style={styles.container}>
-      
-      <SafeAreaView >
+      <SafeAreaView>
         <View style={styles.test}>
-        <Text style={[styles.text, {fontSize: size}]} >{alpha[index]}</Text>
+          <Text style={[styles.text, { fontSize: size }]}>{alpha[index]}</Text>
         </View>
         <View style={styles.container2}>
-        <TouchableOpacity style={[styles.btn, {backgroundColor:'red' }]}>
-          <Text style={styles.text2}>STOP</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.btn, {backgroundColor:'green'}]} onPress={()=>{Nextslide()}}>
-          <Text style={styles.text2}>NEXT</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={[styles.btn, { backgroundColor: "red" }]}>
+            <Text style={styles.text2}>STOP</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.btn, { backgroundColor: "green" }]}
+            onPress={() => {
+              Nextslide();
+            }}
+          >
+            <Text style={styles.text2}>NEXT</Text>
+          </TouchableOpacity>
         </View>
       </SafeAreaView>
     </View>
-  )
-}
+  );
+};
 
 export default Snellers;
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,     
+  container: {
+    flex: 1,
     backgroundColor: COLORS.secondary,
-    alignItems:'center',
-    justifyContent:'center',
-    },
-    test:{
-     
-      height:320,
-      width:320,
-      backgroundColor:'white',
-      alignItems:'center',
-      justifyContent:'center'
-    },
-    text:{
-      fontWeight:'bold',
-    },
-    container2:{
-     flexDirection: 'row',
-     justifyContent:'space-between',
-     margin:20,
-    },
-    btn: {
-      height:40,
-      width:80,
-      alignItems:'center',
-      justifyContent:'center',
-      borderRadius:7,
-      
-    },
-    text2:{
-      color:'white',
-      fontWeight:'bold',
-      fontSize:20,
-    },
-})
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  test: {
+    height: 320,
+    width: 320,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontWeight: "bold",
+  },
+  container2: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 20,
+  },
+  btn: {
+    height: 40,
+    width: 80,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 7,
+  },
+  text2: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+});
