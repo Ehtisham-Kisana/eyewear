cd66bba9fc387033690f8b3a685b3f1603eead9d
Ehtisham Ahmad Kisana <ehtishamkisana@gmail.com>
2023-03-03 21:08:21 +0500

    started implementing Color Plate test

---
+++ components/Eyecomponents/ColorPlateMethod/ColorPlate.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ColorPlate = () => {
+  return (
+    <View>
+      <Text>ColorPlate</Text>
+    </View>
+  )
+}
+
+export default ColorPlate
+
+const styles = StyleSheet.create({})
---
+++ components/Eyecomponents/ColorPlateMethod/ColorPlateInstruction.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ColorPlateInstruction = () => {
+  return (
+    <View>
+      <Text>ColorPlateInstruction</Text>
+    </View>
+  )
+}
+
+export default ColorPlateInstruction
+
+const styles = StyleSheet.create({})
