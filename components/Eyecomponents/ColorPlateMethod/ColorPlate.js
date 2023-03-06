28540a6124dccd899b11a0d0195c7cf78c5a8656
Ehtisham Ahmad Kisana <ehtishamkisana@gmail.com>
2023-03-06 16:38:16 +0500

    adding score

--- components/Eyecomponents/ColorPlateMethod/ColorPlate.js
+++ components/Eyecomponents/ColorPlateMethod/ColorPlate.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
-import { View, Image, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image
+} from "react-native";
 
 
 const COLORS = {
@@ -11,24 +17,25 @@ const COLORS = {
 
 // Color Plate Test Images from the internet
 const images = [
-  {uri: require('../../../assets/ColorPlates/one.png'), msg:'All people should see a number 12, including those with total color blindness. If someone said they cant see something, or saw something else they are fibbing' },
-  {uri: require('../../../assets/ColorPlates/two.png'), msg:'Those with normal color vision see an 8. Those with red-green deficiencies see a 3. Those with total color blindness and weakness are not able to read any numeral.' },
-  {uri: require('../../../assets/ColorPlates/three.png'), msg:'Those with normal color vision see an 6. Those with red-green deficiencies see a 5. Those with total color blindness and weakness are not able to read any numeral.' },
-  {uri: require('../../../assets/ColorPlates/four.png'), msg:'Those with normal color vision see a 29. Those with red-green deficiencies see a 70. Those with total color blindness and weakness are not able to read any numeral.' },
-  {uri: require('../../../assets/ColorPlates/five.png'), msg:'Those with normal color vision see a 57. Those with red-green deficiencies see a 35. Those with total color blindness and weakness are not able to read any numeral.' },
-  {uri: require('../../../assets/ColorPlates/six.png'), msg:'Those with normal color vision see a 5. Those with red-green deficiencies see a 2. Those with total color blindness and weakness are not able to read any numeral.' },
-  {uri: require('../../../assets/ColorPlates/seven.png'), msg:'Those with normal color vision see a 2. The majority of those with colorvision deficiencies are not able to read it or read it incorrectly.' },
-  {uri: require('../../../assets/ColorPlates/eight.png'), msg:'Those with normal color vision see a 97. The majority of those with colorvision deficiencies are not able to read it or read it incorrectly.' },
-  {uri: require('../../../assets/ColorPlates/nine.png'), msg:'Those with normal color vision see a 7. The majority of those with colorvision deficiencies are not able to read it or read it incorrectly.' },
-  {uri: require('../../../assets/ColorPlates/ten.png'), msg:'Those with normal color vision should see a 26. Red color blind (protanopia) people will see a 6, mild red color blind people (mild protanomaly) will also faintly see a number 2. Green color blind (deuteranopia) people will see a 2, mild green color blind people (mild deuteranomaly) may also faintly see a number 6.' },
-  {uri: require('../../../assets/ColorPlates/eleven.png'), msg:'Those with normal color vision should see a 35. Red color blind (protanopia) people will see a 5, mild red color blind people (mild protanomaly) will also faintly see a number 3. Green color blind (deuteranopia) people will see a 3, mild green color blind people (mild deuteranomaly) may also faintly see a number 5.' },  
+  {uri: require('../../../assets/ColorPlates/one.png'),correct:'12', msg:'All people should see a number 12, including those with total color blindness. If someone said they cant see something, or saw something else they are fibbing' },
+  {uri: require('../../../assets/ColorPlates/two.png'),correct:'12', msg:'Those with normal color vision see an 8. Those with red-green deficiencies see a 3. Those with total color blindness and weakness are not able to read any numeral.' },
+  {uri: require('../../../assets/ColorPlates/three.png'),correct:'12', msg:'Those with normal color vision see an 6. Those with red-green deficiencies see a 5. Those with total color blindness and weakness are not able to read any numeral.' },
+  {uri: require('../../../assets/ColorPlates/four.png'),correct:'12', msg:'Those with normal color vision see a 29. Those with red-green deficiencies see a 70. Those with total color blindness and weakness are not able to read any numeral.' },
+  {uri: require('../../../assets/ColorPlates/five.png'),correct:'12', msg:'Those with normal color vision see a 57. Those with red-green deficiencies see a 35. Those with total color blindness and weakness are not able to read any numeral.' },
+  {uri: require('../../../assets/ColorPlates/six.png'), correct:'12',msg:'Those with normal color vision see a 5. Those with red-green deficiencies see a 2. Those with total color blindness and weakness are not able to read any numeral.' },
+  {uri: require('../../../assets/ColorPlates/seven.png'),correct:'12', msg:'Those with normal color vision see a 2. The majority of those with colorvision deficiencies are not able to read it or read it incorrectly.' },
+  {uri: require('../../../assets/ColorPlates/eight.png'),correct:'12', msg:'Those with normal color vision see a 97. The majority of those with colorvision deficiencies are not able to read it or read it incorrectly.' },
+  {uri: require('../../../assets/ColorPlates/nine.png'),correct:'12', msg:'Those with normal color vision see a 7. The majority of those with colorvision deficiencies are not able to read it or read it incorrectly.' },
+  {uri: require('../../../assets/ColorPlates/ten.png'),correct:'12', msg:'Those with normal color vision should see a 26. Red color blind (protanopia) people will see a 6, mild red color blind people (mild protanomaly) will also faintly see a number 2. Green color blind (deuteranopia) people will see a 2, mild green color blind people (mild deuteranomaly) may also faintly see a number 6.' },
+  {uri: require('../../../assets/ColorPlates/eleven.png'),correct:'12', msg:'Those with normal color vision should see a 35. Red color blind (protanopia) people will see a 5, mild red color blind people (mild protanomaly) will also faintly see a number 3. Green color blind (deuteranopia) people will see a 3, mild green color blind people (mild deuteranomaly) may also faintly see a number 5.' },  
 ];
 
 
 const ColorPlate = () => {
   const [currentImage, setCurrentImage] = useState(0);
 
-  const handleImagePress = () => {
+
+  const Nextslide = () => {
     if (currentImage === images.length - 1) {
       setCurrentImage(0);
     } else {
@@ -38,12 +45,25 @@ const ColorPlate = () => {
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={handleImagePress}>
-        <Image source={images[currentImage].uri} style={styles.image} />
-      </TouchableOpacity>
-      <Text style={styles.infoText}>
-      {images[currentImage].msg}
-      </Text>
+      
+
+          <Image style={styles.test} source={images[currentImage].uri} />
+  
+        <View style={styles.container2}>
+          <TouchableOpacity
+            style={[styles.btn, { backgroundColor: "green" }]}
+            onPress={() => {
+              Nextslide();
+            }}
+          >
+            <Text style={styles.text2}>NEXT</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.container3}>
+        <Text style={styles.text3}> {images[currentImage].msg} </Text>
+
+        </View>
+     
     </View>
   );
 };
@@ -51,19 +71,51 @@ const ColorPlate = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: COLORS.secondary,
+    alignItems: "center",
+    justifyContent: "center",
   },
-  image: {
-    width: 330,
+  test: {
     height: 330,
-    resizeMode: 'contain',
+    width: 330,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  infoText: {
-    marginTop: 20,
-    fontSize: 16,
-    textAlign: 'center',
+
+  container2: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 20,
   },
+  btn: {
+    height: 40,
+    width: 80,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 7,
+  },
+  text2: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  container3: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 20,
+    padding:20,
+    borderColor:COLORS.dark,
+    borderWidth:3,
+    borderRadius:18,
+    backgroundColor:COLORS.primary,
+  },
+  text3: {
+    color: 'white',
+    fontSize: 20,
+  },
+
 });
 
 export default ColorPlate;
