5b06e2be0b7f3835af77df744aa276bb7df26923
Ehtisham Ahmad Kisana <ehtishamkisana@gmail.com>
2023-03-10 22:25:58 +0500

    AMD instruction

---
+++ components/Eyecomponents/AmdMethod/AmdInstruction.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AmdInstruction = () => {
+  return (
+    <View>
+      <Text>AmdInstruction</Text>
+    </View>
+  )
+}
+
+export default AmdInstruction
+
+const styles = StyleSheet.create({})
