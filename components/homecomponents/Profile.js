2d284244335f36af5e00b974c265bdf32ef63f44
Ehtisham Ahmad Kisana <ehtishamkisana@gmail.com>
2023-03-17 20:36:12 +0500

    working on profile section

--- components/homecomponents/Profile.js
+++ components/homecomponents/Profile.js
@@ -1,12 +1,28 @@
 import * as React from 'react';
-import { useEffect } from 'react';
-import { Text, View, StyleSheet, Button, TouchableOpacity, Image } from 'react-native';
-import { auth } from '../../firebase/firebase.config';
+import { useEffect, useState } from 'react';
+import { Text, View, StyleSheet, Button, TouchableOpacity, Image, TextInput, AsyncStorage } from 'react-native';
+import { auth,db } from '../../firebase/firebase.config';
 import { signOut } from 'firebase/auth';
+import { collection, addDoc } from "firebase/firestore"; 
+import { async } from '@firebase/util';
+
 
 
 const Profile = ({navigation}) =>{
+
+  const [name, setName] = useState(null);
+  const [address, setAddress] = useState(null);
+  const [phone, setPhone] = useState(null);
  
+  const saveData = async()=>{
+    // Add a new document with a generated id.
+const docRef = await addDoc(collection(db, "users"), {
+  Name:name,
+  Address:address,
+  Phone:phone
+});
+console.log("Document written with ID: ", docRef.id);
+  }
     useEffect(()=>{
         navigation.setOptions({
             headerRight: ()=>(
@@ -40,8 +56,26 @@ const Profile = ({navigation}) =>{
 
     return (
      <View style={styles.container}>
-        <Text>This is Profile Page</Text>
+        <Text style={styles.title}>Profile Screen</Text>
+
+        <TextInput style={styles.input}
+        placeholder="Full Name"
+        onChangeText={(text)=>setName(text)}
+        
+        />
+        <TextInput style={styles.input}
+        placeholder="Phone"
+        onChangeText={(text)=>setPhone(text)}
+        keyboardType="numeric"
+        
+        />
+        <TextInput style={styles.input}
+        placeholder="Address"
+        onChangeText={(text)=>setAddress(text)}
+        
+        />
          
+         <Button title='Submit' onPress={()=> saveData()}></Button>
      </View>
       );
 }
@@ -54,6 +88,25 @@ const styles = StyleSheet.create({
     flex:1,
     flexDirection:'column',
     alignItems:'center',
-    justifyContent:'center',
+    margin:20,
+
+    },
+
+    title:{
+      fontWeight:'bold',
+      fontSize:30,
+      marginBottom:50,
+    },
+
+    input:{
+      borderWidth:1,
+      borderColor:'black',
+      height:50,
+      width:350,
+      padding:10,
+      fontSize:20,
+      borderRadius:9,
+      backgroundColor:'white',
+      margin:10,
     },
 });
