efd3f2ce0d0768b46c1b07f7d45acb843e3891a7
Ehtisham Ahmad Kisana <ehtishamkisana@gmail.com>
2023-03-18 17:50:24 +0500

    Lets create a store for our funky glasses.

---
+++ components/Glasses/Wishlist.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Wishlist = () => {
+  return (
+    <View>
+      <Text>Wishlist</Text>
+    </View>
+  )
+}
+
+export default Wishlist
+
+const styles = StyleSheet.create({})
