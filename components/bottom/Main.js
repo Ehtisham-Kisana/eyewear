9eae1cbc54fb75ea9b46dbda5acc9085aed1aea6
Ehtisham Ahmad Kisana <ehtishamkisana@gmail.com>
2023-03-31 20:52:26 +0500

    hd

--- components/bottom/Main.js
+++ components/bottom/Main.js
@@ -76,6 +76,7 @@ const Main = () => {
                   }}
                 >
                 
+                
                   <Text
                     style={{
                       marginLeft: 10,
